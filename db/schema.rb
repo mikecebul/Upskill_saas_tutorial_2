5124d54d98921e384e828bf333da654dbc177d87
Mike Cebulski <mikecebul@gmail.com>
2018-09-12 01:12:44 +0000

    Added plan vars and params to home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180911220135) do
+ActiveRecord::Schema.define(version: 20180911235028) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
